e2d8d78c9e1db6c3dd087130db718d69f69548e7
Dorota Gomulec <doramed8@wp.pl>
2023-12-18 10:33:09 +0100

    VisitController.java : hateoas added

--- src/main/java/pl/dorota/forphysio/VisitController.java
+++ src/main/java/pl/dorota/forphysio/VisitController.java
@@ -3,6 +3,7 @@ package pl.dorota.forphysio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,29 +20,49 @@ public class VisitController {
         this.visitRepository = visitRepository;
     }
 
-    @PostMapping ("/{id}")
+    @PostMapping("/{id}")
     public int addVisit(@PathVariable int id) {
-       return visitRepository.addVisit( id );
+        return visitRepository.addVisit( id );
     }
 
 
-    @GetMapping ("")
-    public CollectionModel<EntityModel<Visit>> getVisits(){
+    @GetMapping("")
+    public CollectionModel<EntityModel<Visit>> getVisits() {
         List<Visit> visitList = visitRepository.getVisits();
         List<EntityModel<Visit>> entityVisitList = visitList
                 .stream()
                 .map( visit -> EntityModel.of( visit ) )
                 .toList();
-        return CollectionModel.of( entityVisitList, WebMvcLinkBuilder.linkTo(VisitController.class).withSelfRel() );
+
+        for (EntityModel<Visit> visitEntityModel : entityVisitList) {
+            int visitId = visitEntityModel.getContent().getVisitId();
+            visitEntityModel.add( WebMvcLinkBuilder.linkTo( VisitController.class ).slash( visitId ).withRel( "delete_visit" ) );
+        }
+
+        return CollectionModel.of( entityVisitList,
+                WebMvcLinkBuilder.linkTo( VisitController.class ).withSelfRel() );
     }
 
-    @GetMapping ("/{patientId}")
-    public List<Visit> getPatientsVisits(@PathVariable int patientId) {
-        return visitRepository.getPatientsVisit(patientId);
+    @GetMapping("/{patientId}")
+    public CollectionModel<EntityModel<Visit>> getPatientsVisits(@PathVariable int patientId) {
+        List<Visit> visitList = visitRepository.getPatientsVisit( patientId );
+        List<EntityModel<Visit>> visitEntityList = visitList
+                .stream()
+                .map( visit -> EntityModel.of( visit ) ).toList();
+
+        for (EntityModel<Visit> visitEntityModel : visitEntityList) {
+            visitEntityModel.add( WebMvcLinkBuilder.linkTo( VisitController.class ).slash( patientId ).withRel( "delete_visit" ),
+                    WebMvcLinkBuilder.linkTo( VisitController.class ).withSelfRel().withRel( "all_visits_all_patients" ));
+        }
+
+        Link link = (Link) WebMvcLinkBuilder.linkTo( VisitController.class ).slash( patientId ).withSelfRel();
+
+        return CollectionModel.of( visitEntityList, link );
     }
 
-    @DeleteMapping ("/{visitId}")
-    public int deleteByVisitID(@PathVariable int visitId) {
-        return visitRepository.deleteByVisitID(visitId);
+        @DeleteMapping("/{visitId}")
+        public int deleteByVisitID ( @PathVariable int visitId){
+            return visitRepository.deleteByVisitID( visitId );
+        }
     }
-}
+
