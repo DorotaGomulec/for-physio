b4a174d6c63555d4c18113e70a8aff2e09fe243c
Dorota Gomulec <doramed8@wp.pl>
2024-01-03 14:18:19 +0100

    data validation added

--- src/main/java/pl/dorota/forphysio/Patient.java
+++ src/main/java/pl/dorota/forphysio/Patient.java
@@ -1,6 +1,10 @@
 package pl.dorota.forphysio;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.util.Date;
 
@@ -10,7 +14,10 @@ import java.util.Date;
 
 public class Patient {
     private int id;
+    @NotBlank (message = "Field name can not be blank")
     private String name;
+    @Min( value = 0 , message = "Minimum value of age has to be 0" )
+    @NumberFormat
     private Integer age;
     private Gender gender;
     private String phoneNumber;
@@ -19,5 +26,9 @@ public class Patient {
     private int numberOfVisits;
     private String lastVisit;
 
+    public Patient(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
 }
 
